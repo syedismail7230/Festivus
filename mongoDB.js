7c271003f8fe7b7727f66ca12e3760601b891ee1
syedismail7230 <syedismailart@gmail.com>
2023-11-08 15:54:40 +0530

    Festivus initial commit

---
+++ mongoDB.js
@@ -0,0 +1,15 @@
+
+db.users.insertOne({
+    "USN": "1HK21AI059",
+    "googleId": "1hk21ai059@hkbk.edu.in",
+    "officialMailId": "1hk21ai059@hkbk.edu.in",
+    "firstName": "Syed Ismail",
+    "lastName": "Zabiulla"
+  });
+  
+  db.users.insertOne({
+    "USN": "1HK21AI0",
+    "officialMailId": "",
+    "firstName": "",
+    "lastName": ""
+  });
